540030cf70dfbc96117c0151c3f6766e09ed1d12
Sayed Salem <sayedsalem767@gmail.com>
2024-12-01 20:12:04 +0200

    nothing

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -3,10 +3,7 @@ import os
 import random
 import string
 class BaseController:
-
-
     def __init__(self):
-
         self.app_settings = get_settings()
         self.base_dir = os.path.dirname(os.path.dirname(__file__))
         print(self.base_dir)
