e687843ffcd1b3df51c74f24722d8d41753640db
Sayed Salem <sayedsalem767@gmail.com>
2025-01-30 16:57:45 +0200

    removed on_event decorator and used lifespan

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from routes import base, data
 from motor.motor_asyncio import AsyncIOMotorClient
-
+from stores.llm import LLMFactoryProvider
 # this motor client is used to connect to the mongodb database
 # perform operations on it (it is async and fast api is async so it is used to make the operations faster)
 
@@ -10,7 +10,6 @@ from helpers.config import get_settings
 app = FastAPI()
 
 
-@app.on_event('startup')
 async def startup_db_client():
     # this func is used to make the connection to the mongodb database
     settings = get_settings()
@@ -18,12 +17,28 @@ async def startup_db_client():
     app.mongo_conn = AsyncIOMotorClient(settings.MONGO_URL)
     app.db_client = app.mongo_conn[settings.MONGO_DATABASE]
 
+    llm_factory_provider = LLMFactoryProvider(config=settings)
 
-@app.on_event('shutdown')
+    # generation model
+    app.generation_model = llm_factory_provider.create_provider(
+        provider=settings.GENERATION_BACKEND)
+    app.generation_model.set_generation_model(
+        model_id=settings.GENERATION_MODEL_ID)
+
+    # embedding model
+    app.embedding_model = llm_factory_provider.create_provider(
+        provider=settings.EMBEDDING_BACKEND)
+    app.embedding_model.set_embedding_model(
+        model_id=settings.EMBEDDING_MODEL_ID)
+
+
 async def shutdown_db_client():
     # this func is used to close the connection to the mongodb database
     app.mongo_conn.close()
 
+
+app.router.lifespan.on_startup.append(startup_db_client)
+app.router.lifespan.on_shutdown.append(shutdown_db_client)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
