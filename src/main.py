ec513ce080785dca68b93391c61de2c7c27f4a7b
Sayed Salem <sayedsalem767@gmail.com>
2025-02-05 19:59:43 +0200

    Added NLP router to the application Added vectordb client to use it in the whole application

--- src/main.py
+++ src/main.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI
-from routes import base, data
+from routes import base, data, nlp
 from motor.motor_asyncio import AsyncIOMotorClient
-from stores.llm import LLMFactoryProvider
 # this motor client is used to connect to the mongodb database
 # perform operations on it (it is async and fast api is async so it is used to make the operations faster)
+
+from stores.llm import LLMFactoryProvider
+from stores.vectordb import VectorDBFactoryProvider
 
 from helpers.config import get_settings
 from contextlib import asynccontextmanager
 
+from models import ProjectModel, ChunkModel
 
-async def startup_db_client():
+
+async def startup_spam():
     # this func is used to make the connection to the mongodb database
     settings = get_settings()
 
@@ -17,37 +21,54 @@ async def startup_db_client():
     app.db_client = app.mongo_conn[settings.MONGO_DATABASE]
 
     llm_factory_provider = LLMFactoryProvider(config=settings)
+    vectordb_factory_provider = VectorDBFactoryProvider(config=settings)
 
-    # generation model
+    # Project model
+    app.project_model = await ProjectModel.create_instance(
+        db_client=app.db_client
+    )
+
+    # Chunk model
+    app.chunk_model = await ChunkModel.create_instance(
+        db_client=app.db_client
+    )
+
+    # Generation model
     app.generation_model = llm_factory_provider.create_provider(
         provider=settings.GENERATION_BACKEND)
     app.generation_model.set_generation_model(
         model_id=settings.GENERATION_MODEL_ID)
 
-    # embedding model
+    # Embedding model
     app.embedding_model = llm_factory_provider.create_provider(
         provider=settings.EMBEDDING_BACKEND)
     app.embedding_model.set_embedding_model(
         embedding_model_id=settings.EMBEDDING_MODEL_ID,
         embedding_size=settings.EMBEDDING_SIZE)
 
+    # VectorDB
+    app.vectordb_client = vectordb_factory_provider.create_provider(
+        provider=settings.VECTOR_DB_BACKEND
+    )
+    app.vectordb_client.connect()
 
-async def shutdown_db_client():
+
+async def shutdown_spam():
     # this func is used to close the connection to the mongodb database
     app.mongo_conn.close()
+    app.vectordb_client.disconnect()
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await startup_db_client()
+    await startup_spam()
     yield
-    await shutdown_db_client()
+    await shutdown_spam()
 
 
 app = FastAPI(lifespan=lifespan)
 
-# app.router.lifespan.on_startup.append(startup_db_client)
-# app.router.lifespan.on_shutdown.append(shutdown_db_client)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
+app.include_router(nlp.nlp_router)
