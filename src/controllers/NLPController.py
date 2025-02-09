ef5301bc34c91582ee8f843c66d3bc5a1f957cf9
Sayed Salem <sayedsalem767@gmail.com>
2025-02-09 20:33:28 +0200

    Added the template parser as a parameter and the answer rag question as a new function to answer the user query

--- src/controllers/NLPController.py
+++ src/controllers/NLPController.py
@@ -1,5 +1,6 @@
 from .BaseController import BaseController
 from stores.llm import LLMFactoryProvider
+from stores.llm.templates import TemplateParser
 from stores.vectordb import VectorDBFactoryProvider
 
 from models.db_schemas import DataChunk
@@ -14,12 +15,14 @@ class NLPController(BaseController):
 
     def __init__(self, vectordb_client: VectorDBFactoryProvider,
                  generation_model: LLMFactoryProvider,
-                 embedding_model: LLMFactoryProvider):
+                 embedding_model: LLMFactoryProvider,
+                 template_parser: TemplateParser):
         super().__init__()
 
         self.vectordb_client = vectordb_client
         self.generation_model = generation_model
         self.embedding_model = embedding_model
+        self.template_parser = template_parser
 
     def create_collection_name(self, project_id: str):
         return f'collection_{project_id}'.strip()
@@ -86,6 +89,66 @@ class NLPController(BaseController):
         if not results:
             return False
 
-        return json.loads(
-            json.dumps(results, default=lambda x: x.__dict__)
+        return results
+
+    def answer_rag_question(self, project_id: str, query: str, limit: int = 10):
+
+        full_prompt, chat_history, answer = None, None, None
+
+        retrieved_documents = self.search_vector_db_collection(
+            project_id=project_id,
+            text=query,
+            limit=limit
         )
+
+        if not retrieved_documents or len(retrieved_documents) == 0:
+            return full_prompt, chat_history, answer
+
+        system_prompt = self.template_parser.get_template(
+            group='rag',
+            key='system_prompt',
+        )
+
+        documents_prompt = '\n'.join(
+            [
+                self.template_parser.get_template(
+                    group='rag',
+                    key='document_prompt',
+                    vars={
+                        'doc_num': idx,
+                        'doc_content': doc.text,
+                        'doc_metadata': doc.metadata
+                    }
+                )
+                for idx, doc in enumerate(retrieved_documents, 1)
+            ]
+        )
+
+        footer_prompt = self.template_parser.get_template(
+            group='rag',
+            key='footer_prompt',
+            vars={
+                'query': query
+            }
+        )
+
+        chat_history = [
+            self.generation_model.construct_prompt(
+                prompt=system_prompt,
+                role=self.generation_model.enums.SYSTEM.value
+            )
+        ]
+
+        full_prompt = '\n'.join(
+            [
+                documents_prompt,
+                footer_prompt
+            ]
+        )
+
+        answer = self.generation_model.generate_text(
+            prompt=full_prompt,
+            chat_history=chat_history,
+        )
+
+        return full_prompt, chat_history, answer
