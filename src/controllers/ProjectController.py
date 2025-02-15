a823de16a71dae99d258e012c5d2b4a9f2c70b4c
Sayed Salem <sayedsalem767@gmail.com>
2025-02-15 22:12:56 +0200

    Solved problem of changing project_id from str to int and translate it again to str to generate the path

--- src/controllers/ProjectController.py
+++ src/controllers/ProjectController.py
@@ -15,11 +15,11 @@ class ProjectController(BaseController):
     def __init__(self):
         super().__init__()
 
-    def get_project_path(self, project_id: str):
+    def get_project_path(self, project_id: int):
 
         project_dir = os.path.join(
             self.file_dir,
-            project_id
+            str(project_id)
         )
         if not os.path.exists(project_dir):
             os.makedirs(project_dir)
